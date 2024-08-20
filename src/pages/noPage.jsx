6b8d2a9ffe24c1e1ca7b00649c2dc4655ea9448d
Thandekaportiap <thandekaportiap05@gmail.com>
2024-08-20 16:19:58 +0200

    page

--- src/pages/noPage.jsx
+++ src/pages/noPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {NoPages} from '../assets/nopage.jpg'
 
 export default function NoPage() {
   return (
     <div>
       <h1>404 : Sorry page not found</h1>
+      <img src={NoPages} alt="" />
     </div>
   )
 }
