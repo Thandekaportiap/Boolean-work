004273df23e7b13ca338a245c446259b931f02da
Thandekaportiap <thandekaportiap05@gmail.com>
2024-08-21 16:01:21 +0200

    buttons

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -6,10 +6,12 @@ const landing = () => {
    <>
    <div style={{marginLeft:"5%", marginTop:"7%"}}>
     <p style={{color:"#e3e3e3"}}>-free 30days trial</p>
-    <h1 style={{lineHeight:"4rem", fontSize:"400%", fontWeight:"bold"}}>The best way<br/> to showcase<br/> your project </h1>
+    <h1 style={{lineHeight:"4rem", fontSize:"400%", fontWeight:"bold", margin:"2% 0"}}>The best way<br/> to showcase<br/> your project </h1>
     <h4>Here you can put your short description about your project</h4>
+    <div style={{marginTop:"2%"}}>
     <button style={{backgroundColor:"blue", color:"whitesmoke", borderRadius:"4px", padding:"8px 18px", marginRight:"3%"}}>Try for free</button>
-    <button>See how it works</button>
+    <button style={{backgroundColor:"whitesmoke", color:"black",borderRadius:"4px",padding:"8px 26px", }}>See how it works</button>
+    </div>
    </div>
    <div style={{position:"absolute", left:"45%", top:"15%"}}>
         <img src={Hero} alt="" />
