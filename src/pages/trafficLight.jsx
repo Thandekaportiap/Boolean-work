d322f5be2cf47c894d40f88d2000c9d8c28cc999
Thandekaportiap <thandekaportiap05@gmail.com>
2024-08-26 10:09:42 +0200

    traffic

--- src/pages/trafficLight.jsx
+++ src/pages/trafficLight.jsx
@@ -1,32 +1,58 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function TrafficLight() {
-   const [color1, setColor1 ] = useState("black")
-   const [color2, setColor2 ] = useState("black")
-   const [color3, setColor3 ] = useState("black")
-  
+  const [light, setLight] = useState('red');
 
-    const changeColorRed = (e) => {
-        setColor1( "red" )
-    };
-    const changeColorOrange = (e) => {
-      setColor2( "orange" )
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      switch (light) {
+        case 'red':
+          setLight('green');
+          setTimeout(()=> setLight('yellow'), 3000)
+          break;
+        case 'yellow':
+          setLight('red');
+          break;
+        case 'green':
+          setLight('yellow');
+          break;
+        default:
+          break;
+      }
+    }, getDuration(light));
+    return ()=> clearTimeout(timer);
+  }, [light])
 
-  };const changeColorGreen = (e) => {
-    setColor3( "green" )
-};
+  const getDuration = (currentLight) =>{
+    switch (currentLight) {
+      case 'red':
+        return 4000;
+      case 'yellow':
+        return 500;
+      case 'green':
+        return 3000;
+      default:
+        return 0;
+    }
+  }
  
 
   return (
    <>
-   <div style={{ display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center"}}>
+   {/* <div style={{ display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center"}}>
     <div style={{backgroundColor:"#747E8B",width:"12%",height:"200px",marginTop:"20%" ,display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center"}}>
       <div style={{backgroundColor:color1,width:"35%",height:"60px",borderRadius:"50%"}} onClick={changeColorRed}></div>
       <div style={{backgroundColor:color2,width:"35%",height:"60px",borderRadius:"50%"}} onClick={changeColorOrange}></div>
       <div style={{backgroundColor:color3,width:"35%",height:"60px",borderRadius:"50%"}} onClick={changeColorGreen}></div>
       </div>
-      </div>
+      </div> */}
+
+<div className='traffic-light'>
+      <div className={`light red ${light === 'red'? 'active': ''}`}></div>
+      <div className={`light yellow ${light === 'yellow'? 'active': ''}`}></div>
+      <div className={`light green ${light === 'green'? 'active': ''}`}></div>
+    </div>
    </>
   )
 }
